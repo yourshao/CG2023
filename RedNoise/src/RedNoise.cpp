c6b5fd337d1b98f35c21f983bda8ae9781fec392
yourshao <wohelijiaweishi@icloud.com>
2023-10-08 13:43:13 +0100

    grey spectrum

--- RedNoise/src/RedNoise.cpp
+++ RedNoise/src/RedNoise.cpp
@@ -7,18 +7,47 @@
 #define WIDTH 320
 #define HEIGHT 240
 
+
+
+std::vector<float> interpolateSingleFloats(float from, float to, int numberOfValues){
+    std::vector<float> result;
+
+    float difference = ( to - from ) /( numberOfValues-1 )  ;
+    for (int a = 0 ;a < numberOfValues; a++){
+        result.push_back(from + a * difference);
+    }
+    return result;
+}
+
+
+
 void draw(DrawingWindow &window) {
 	window.clearPixels();
+
+
 	for (size_t y = 0; y < window.height; y++) {
-		for (size_t x = 0; x < window.width; x++) {
-			float red = rand() % 256;
-			float green = 0.0;
-			float blue = 0.0;
-			uint32_t colour = (255 << 24) + (int(red) << 16) + (int(green) << 8) + int(blue);
+        std::vector<float> result;
+        result = interpolateSingleFloats(0, 255, WIDTH);
+        for (size_t x = 0; x < window.width; x++) {
+            //this is original red noise
+//			float red = rand() % 256;
+//			float green = 0.0;
+//			float blue = 0.0;
+//			uint32_t colour = (255 << 24) + (int(red) << 16) + (int(green) << 8) + int(blue);
+
+// make grey spectrum
+            float red = 255-result.front();
+            float green = 255-result.front();
+            float blue = 255-result.front();
+            result.erase(result.begin());
+            uint32_t colour = (255 << 24) + (int(red) << 16) + (int(green) << 8) + int(blue);
+
 			window.setPixelColour(x, y, colour);
 		}
 	}
 }
+
+
 
 
 
@@ -35,35 +64,18 @@ void handleEvent(SDL_Event event, DrawingWindow &window) {
 	}
 }
 
-std::vector<float> interpolateSingleFloats(float from, float to, int numberOfValues){
-    std::vector<float> result;
-
-    float difference = ( to - from ) /( numberOfValues-1 )  ;
-    for (int a = 0 ;a < numberOfValues; a++){
-        result.push_back(from + a * difference);
-    }
-    return result;
-}
 
 
 
 int main(int argc, char *argv[]) {
 	DrawingWindow window = DrawingWindow(WIDTH, HEIGHT, false);
 	SDL_Event event;
-
+    // test for the interpolateSingleFloats() function
 //    std::vector<float> result;
 //    result = interpolateSingleFloats(2.2, 8.5, 7);
 //    for(size_t i=0; i<result.size(); i++) std::cout << result[i] << " ";
 //    std::cout << std::endl;
 //
-
-
-
-
-
-
-
-
 	while (true) {
 		// We MUST poll for events - otherwise the window will freeze !
 		if (window.pollForInputEvents(event)) handleEvent(event, window);
